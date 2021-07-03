745b1f5576d9a6e211c801a784242a8161308111
Andres Marquez <andres03ruht@gmail.com>
2021-07-03 16:05:45 -0430

    Rutas

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: vendor/bin/heroku-php-apache2 public/css
+web: vendor/bin/heroku-php-apache2 public/
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-    Route::get('/', 'login_register@index');
+
+use Illuminate\Support\Facades\Route;
 
 Route::middleware(['guest'])->group(function () {
     Route::get('/', 'login_register@index');
