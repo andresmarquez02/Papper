fc1cf2d221b2a8c9e6278da901b8cfc2dbafb4f3
Andres Marquez <andres03ruht@gmail.com>
2021-07-05 11:11:33 -0430

    https

--- public/index.php
+++ public/index.php
@@ -51,11 +51,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-// $response = $kernel->handle(
-//     $request = Illuminate\Http\Request::capture()
-// );
 $response = $kernel->handle(
-    $request = App\Custom\Http\Request::capture()
+    $request = Illuminate\Http\Request::capture()
 );
 $response->send();
 
